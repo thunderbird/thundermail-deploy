2a806a4f95f2ef35dddd0a28b722bf2d387443c6
Ryan Jung <rjung@thunderbird.net>
2026-05-28 16:14:30 -0600

    Commentary

--- util/kustomize-build-all.sh
+++ util/kustomize-build-all.sh
@@ -11,14 +11,17 @@
 echo '***** KUSTOMIZE BUILD REPORT *****'
 echo
 
+# Work within the overlays directory
 pushd overlays &> /dev/null
 
+# Discover all overlay directories and try to `kustomize build` them
 FAIL_COUNT=0
 for overlay in $(ls -d *); do
     echo "-- $overlay:"
     output=$(kustomize build $overlay 2>&1 )
     rc=$?
 
+    # Report output on failed builds so we can react
     echo -n 'Build status: '
     if [ $rc -ne 0 ]; then
         FAIL_COUNT=$((FAIL_COUNT+1))
@@ -35,6 +38,8 @@ done
 popd &> /dev/null
 
 echo "Total build failures: $FAIL_COUNT"
+
+# Exit with error code if any failures occurred
 if [ $FAIL_COUNT -gt 0 ]; then
     exit 1
 else
